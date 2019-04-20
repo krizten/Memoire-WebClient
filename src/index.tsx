5ac041d8d6bfb10c203105b06fc22289a601cdfc
Christian Effiong <norexus007@gmail.com>
2019-04-20 07:30:11 +0100

    chore(setup): add node-sass dependency and convert css to scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import 'react-app-polyfill';
+import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
