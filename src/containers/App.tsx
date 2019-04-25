eca321def5ca64bcc7a8553067ce5d3390a09188
Christian Effiong <norexus007@gmail.com>
2019-04-25 05:15:16 +0100

    structure react application

---
+++ src/containers/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/main.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Memoire App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
