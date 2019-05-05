5e99dc7a23912febe243cb938e118af06f4a9cd6
Christian Effiong <norexus007@gmail.com>
2019-05-05 23:09:43 +0100

    feat(UI): add login container

---
+++ src/containers/Login.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+interface State {}
+
+export class Login extends Component<{}, State> {
+  render() {
+    return <div />;
+  }
+}
