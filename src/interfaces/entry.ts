666bdd93d1017218c92a7c8232c4276a628b4552
Christian Effiong <norexus007@gmail.com>
2019-09-11 06:36:15 +0100

    add entry and coordinates interfaces

--- src/interfaces/entry.ts
+++ src/interfaces/entry.ts
@@ -1,1 +1,11 @@
-export interface IEntry {}
+import { Coordinates } from './coordinates';
+
+export interface Entry {
+  id: string;
+  created: Date;
+  updated: Date;
+  title: string;
+  content: string;
+  image?: string;
+  geolocation?: Coordinates;
+}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './entry';
 export * from './DTOs';
 export * from './user';
 export * from './token';
+export * from './coordinates';
